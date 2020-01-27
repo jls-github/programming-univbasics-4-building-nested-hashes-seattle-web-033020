bbe5a0c22e77bf6ab141eb2a68a85c15fed8c465
John Souza <souzajohnlee@gmail.com>
2020-01-27 16:59:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,12 @@ def monopoly_with_third_tier
       :three => " ",
       :four => " "
     },
-    :names => {}
+    :names => {
+      :one => " ",
+      :two => " ",
+      :three => " ",
+      :four => " "
+    }
   }}
 end
 
